aa966d0e13712c9c4fb17553ca6997774aab4039
Marwyn02 <jhunmarwynsumargo@yahoo.com>
2023-12-03 20:16:28 +0800

    Feature: Update Users name

--- components/settings/PersonalDetails.tsx
+++ components/settings/PersonalDetails.tsx
@@ -1,4 +1,8 @@
 import { useSession } from "next-auth/react";
+import { useState } from "react";
+import router from "next/router";
+
+import LoadingPostModal from "../UI/LoadingPostModal";
 
 export default function PersonalDetails({
   name,
@@ -10,8 +14,55 @@ export default function PersonalDetails({
   image: string;
 }) {
   const { data: session } = useSession();
+  const [newName, setNewName] = useState<string>(name);
+
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const [onEditState, setOnEditState] = useState<{
+    name: boolean;
+  }>({
+    name: false,
+  });
+
+  // Handle the toggling function of the post information,
+  // title, location, description
+  const handleEditToggle = (inputName: string) => {
+    setOnEditState((prevEditState) => ({
+      ...prevEditState,
+      [inputName]: !prevEditState[inputName as keyof typeof prevEditState],
+    }));
+  };
+
+  // Update handler of the profile information of the user
+  const submitChangeHandler = async (e: React.FormEvent) => {
+    try {
+      e.preventDefault();
+      setIsLoading(true);
+
+      const response = await fetch("/api/setting/edit", {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify({
+          name: newName,
+          id: (session?.user as { _id: string })?._id as string,
+        }),
+      }).then((r) => r.json());
+
+      if (response.success) {
+        console.log(response.message);
+        router.push(response.path);
+        setIsLoading(false);
+      }
+    } catch (error) {
+      console.error("Failed to update a your profile, ", error);
+      setIsLoading(false);
+    }
+  };
   return (
     <section className="mt-10 px-3 lg:px-0">
+      {isLoading && <LoadingPostModal />}
       <h4 className="text-sm text-gray-700 pb-1 font-semibold border-b">
         Account Preferences
       </h4>
@@ -32,12 +83,73 @@ export default function PersonalDetails({
       </h4>
 
       {/* Name  */}
-      <div className="font-bold text-gray-700 my-5">
-        Display name
-        <p className="font-normal bg-gray-100 rounded-md pl-2 py-1 text-gray-700 mt-1">
-          {name}
-        </p>
-      </div>
+      <section className="my-5">
+        {!onEditState.name ? (
+          <section>
+            <h3 className="font-bold text-gray-700">Display name</h3>
+            <p className="text-sm text-gray-500 font-light">
+              Remember to change your name with your real name, people might not
+              recognize you easily.
+            </p>
+            <div className="flex justify-between bg-gray-100 rounded-md px-2 py-1 mt-3">
+              <p className="font-normal text-gray-700 mt-1">{name}</p>
+              <button
+                type="button"
+                className="border border-gray-400 rounded-full px-3 text-xs font-bold text-gray-700 bg-gray-100 hover:text-gray-900 hover:bg-gray-200 duration-300"
+                onClick={() => handleEditToggle("name")}
+              >
+                Change
+              </button>
+            </div>
+          </section>
+        ) : (
+          <section>
+            <div className="flex justify-between items-center">
+              <h3 className="font-bold text-gray-700">Display name</h3>
+              <button
+                className="font-medium text-sm text-gray-800 hover:underline hover:text-black"
+                onClick={() => {
+                  handleEditToggle("name");
+                  setNewName(name);
+                }}
+              >
+                Cancel
+              </button>
+            </div>
+            <p className="text-sm text-gray-500 font-light">
+              Its important that your name is valid, so other people will
+              recognize you easily.
+            </p>
+            <form onSubmit={submitChangeHandler} method="post">
+              <div className="border px-2  py-1 rounded-lg w-fit my-3 hover:border-black duration-300">
+                <label
+                  htmlFor="name"
+                  className="font-light text-xs text-gray-600"
+                >
+                  Make your new name
+                </label>
+                <input
+                  type="text"
+                  placeholder={name}
+                  value={newName}
+                  name="name"
+                  id="name"
+                  onChange={(e) => setNewName(e.target.value)}
+                  className="block w-[270px] py-1 -mt-1 font-light text-black placeholder:text-gray-400
+                sm:text-base sm:leading-6 focus:ring-0 focus:border-0 focus:outline-none"
+                  required
+                />
+              </div>
+              <button
+                type="submit"
+                className="px-5 py-2 bg-gray-800 text-white rounded-md hover:bg-black font-normal"
+              >
+                Save
+              </button>
+            </form>
+          </section>
+        )}
+      </section>
 
       {/* Image  */}
       <h4 className="text-sm text-gray-700 pb-1 font-semibold border-b mt-16">
