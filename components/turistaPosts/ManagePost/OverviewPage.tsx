c953427b569c45c381d74feefe3582820345d81b
Marwyn02 <jhunmarwynsumargo@yahoo.com>
2023-12-15 16:43:35 +0800

    Added: Implementing post management system feature

---
+++ components/turistaPosts/ManagePost/OverviewPage.tsx
@@ -0,0 +1,29 @@
+import { FC } from "react";
+import { useSession } from "next-auth/react";
+
+import PostOverview from "./Components/PostOverview";
+
+type PostsProps = {
+  posts: {
+    id: string;
+    title: string;
+    location: string;
+    image: string;
+  }[];
+};
+
+const OverviewPage: FC<PostsProps> = ({ posts }) => {
+  const { data: session } = useSession();
+  return (
+    <>
+      <section className="p-32">
+        <h1 className="text-3xl font-semibold text-gray-800">
+          Welcome, {session?.user?.name}!
+        </h1>
+        <PostOverview posts={posts} />
+      </section>
+    </>
+  );
+};
+
+export default OverviewPage;
--- pages/manage/posts.tsx
+++ pages/manage/posts.tsx
@@ -1,12 +1,49 @@
-import { useSession } from "next-auth/react";
-import PostOverview from "@/components/turistaPosts/ManagePost/PostOverview";
+import { FC } from "react";
+import { getSession } from "next-auth/react";
+import Post from "@/models/Post";
 
-export default function posts() {
-  const { data: session } = useSession();
+import OverviewPage from "@/components/turistaPosts/ManagePost/OverviewPage";
 
+type PostsProps = {
+  posts: {
+    id: string;
+    title: string;
+    location: string;
+    image: string;
+  }[];
+};
+
+const ManagePostPage: FC<PostsProps> = ({ posts }) => {
   return (
     <>
-      <PostOverview name={session?.user?.name as string} />
+      <OverviewPage posts={posts} />
     </>
   );
+};
+
+export async function getServerSideProps(context: any) {
+  const session = await getSession(context);
+
+  const userId = (session?.user as { _id: string })?._id as string;
+
+  const userPosts = await Post.find({ user: userId }).sort({ _id: -1 });
+
+  const posts = await Promise.all(
+    userPosts.map(async (post) => {
+      return {
+        id: post._id.toString(),
+        title: post.title,
+        location: post.location,
+        image: post.image[0].image,
+      };
+    })
+  );
+
+  // console.log(posts);
+
+  return {
+    props: { posts },
+  };
 }
+
+export default ManagePostPage;
