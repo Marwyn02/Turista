047ef55097c8b52df9dd0598e238ff2333c0a5ac
Marwyn02 <jhunmarwynsumargo@yahoo.com>
2023-12-29 14:47:50 +0800

    Fix: Removes w-full in buttons

--- components/UI/Buttons/Button.tsx
+++ components/UI/Buttons/Button.tsx
@@ -24,7 +24,7 @@ export const SubmitButton = ({ children, disabled }: TProps) => {
   return (
     <button
       type="submit"
-      className="bg-violet-400 text-white text-sm px-5 py-1.5 w-full border 
+      className="bg-violet-400 text-white text-sm px-5 py-1.5 border 
                   border-transparent rounded-md hover:bg-violet-500 duration-200 focus:outline-none "
       disabled={disabled}
     >
@@ -37,9 +37,9 @@ export const ConfirmButton = ({ children, disabled }: TProps) => {
   return (
     <button
       type="button"
-      className="flex items-center bg-violet-400 text-white text-sm px-4 py-2 w-full border 
+      className="flex items-center bg-violet-400 text-white text-sm px-4 py-2 border 
                 border-transparent rounded-md hover:bg-violet-500 duration-200 font-medium
-                focus:outline-none "
+                focus:outline-none"
     >
       {children}
     </button>
