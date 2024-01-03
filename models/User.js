d714a114e06bbd57960cea5d3c209007d91874e8
Marwyn02 <jhunmarwynsumargo@yahoo.com>
2024-01-03 12:08:25 +0800

    Added: Cover_photo type string in the model

--- models/User.js
+++ models/User.js
@@ -14,6 +14,10 @@ const UserSchema = new mongoose.Schema(
       type: String,
       trim: true,
     },
+    cover_photo: {
+      type: String,
+      trim: true,
+    },
     provider: {
       type: String,
     },
